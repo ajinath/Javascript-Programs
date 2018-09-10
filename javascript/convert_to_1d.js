57877be7a00e19304e357cebca71981420d4b350
Ajinath <ajinathjedhe99@gmail.com>
2018-09-10 15:52:32 +0530

    added alternative solution

--- javascript/convert_to_1d.js
+++ javascript/convert_to_1d.js
@@ -1,14 +1,22 @@
 /* Convert multidiamensinal array to 1D array */
 
+/* Solution 1 */
 arr = [1, 8, 3, 9, [5, [6, 7]]];
-temp = []
-
-function convert_to_1d(arr) {
+function convert_to_1d(arr, temp = []) {
   for (let i = 0; i < arr.length; i++)
     if (arr[i] instanceof Array)
-      convert_to_1d(arr[i]);
+      convert_to_1d(arr[i], temp);
     else
       temp.push(arr[i]);
+  return temp;
+}
+console.log("Result = ", convert_to_1d(arr));
+
+/* Solution 2 */
+var convert_to_1d = (arr, temp=[]) => { 
+  arr.forEach(function(ele){
+    ele instanceof Array ? convert_to_1d(ele, temp) : temp.push(ele);
+  });
   return temp;
 }
 console.log("Result = ", convert_to_1d(arr));
