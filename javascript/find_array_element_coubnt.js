92fcd368bbb699c019d1a329c02e7588359d175a
ajinath <ajinathjedhe99@gmail.com>
2018-09-10 12:41:23 +0530

    removed unnecessary code.

--- javascript/find_array_element_coubnt.js
+++ javascript/find_array_element_coubnt.js
@@ -1,13 +1,7 @@
 arr = [1,4,3,6,9,7,6,1,4,3,3,3];
-arr = arr.sort();
-
- [1, 1, 3, 3, 3, 3, 4, 4, 6, 6, 7, 9]
-
-
-result = {}
+result = {};
 
 for(i=0; i< arr.length; i++) {
-    debugger
     if (!result[i])
       result[i] = 1;
     else
