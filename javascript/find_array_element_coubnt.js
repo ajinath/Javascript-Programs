d6af5d4f6f5bdad41f3ea9140c6c1af27c47a39d
ajinath <ajinathjedhe99@gmail.com>
2018-09-10 12:47:15 +0530

    added comment

--- javascript/find_array_element_coubnt.js
+++ javascript/find_array_element_coubnt.js
@@ -1,4 +1,6 @@
+// Given an array to find the occurence of element in an array.
 arr = [1,4,3,6,9,7,6,1,4,3,3,3];
+// OUTPUT - {1: 2, 3: 4, 4: 2, 6: 2, 7: 1, 9: 1}
 result = {};
 
 for(i=0; i< arr.length; i++) {
