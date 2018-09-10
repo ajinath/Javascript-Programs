bc794ec644b55f739c0a32a4953155a522da1262
Ajinath <ajinathjedhe99@gmail.com>
2018-09-10 17:48:05 +0530

    added desc sort with arrow function

--- javascript/sorting.js
+++ javascript/sorting.js
@@ -3,12 +3,14 @@ arr = [2,1,5,9,4];
 // sort array in ascending order
 arr.sort();
 
-result = a.sort(function(a,b){
+result = arr.sort(function(a,b){
   return a < b ? -1 : 1 
 });
 
 // sort array in desc order
-
-result = a.sort(function(a,b){
+result = arr.sort(function(a,b){
   return a > b ? -1 : 1 
 })
+
+// sort array with arrow function
+result = arr.sort((a,b) =>  a > b ? -1 : 1);
